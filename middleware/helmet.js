bc8daeb0b1ca90ebb61b068aa5113b790156fc60
Michela <michela@Michelas-MacBook-Pro.local>
2026-08-11 14:43:20 +1000

    helmet fontSrc

--- middleware/helmet.js
+++ middleware/helmet.js
@@ -8,7 +8,7 @@ const helmetConfig = helmet({
             styleSrc: [ "'self'", "'unsafe-inline'", "https://code.jquery.com", "https://cdn.jsdelivr.net", "https://unpkg.com", "https://sandbox.web.squarecdn.com", "https://web.squarecdn.com", "https://cdnjs.cloudflare.com" ],
             imgSrc: [ "'self'", "data:", "https://res.cloudinary.com", "https://*.tile.openstreetmap.org", "https://unpkg.com", "https://sandbox.web.squarecdn.com", "https://web.squarecdn.com", "https://maps.gstatic.com", "https://www.gstatic.com" ],
             mediaSrc: [ "'self'", "https://res.cloudinary.com" ],
-            fontSrc: [ "'self'", "https://cdnjs.cloudflare.com", "https://fonts.gstatic.com" ],
+            fontSrc: [ "'self'", "https://cdnjs.cloudflare.com", "https://fonts.gstatic.com", "https://square-fonts-production-f.squarecdn.com", "https://d1g145x70srn7h.cloudfront.net", "https://cash-f.squarecdn.com", ],
             connectSrc: [ "'self'", "https://sandbox.web.squarecdn.com", "https://pci-connect.squareupsandbox.com", "https://*.squareup.com", "https://*.squareupsandbox.com", "https://web.squarecdn.com", "https://connect.squareup.com", "https://maps.googleapis.com", "https://google.com", "https://www.google.com", "https://pay.google.com", "https://*.apple-pay.apple.com" ],
             frameSrc: [ "'self'", "https://sandbox.web.squarecdn.com", "https://web.squarecdn.com", "https://pay.google.com", "https://*.squareup.com", "https://*.squareupsandbox.com" ]
         }
