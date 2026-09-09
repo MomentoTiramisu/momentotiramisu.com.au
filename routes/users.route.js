ca536d952711248942b21b2718f5ccc6e498cfb8
Michela <michela@Michelas-MacBook-Pro.local>
2026-09-09 19:43:54 +1000

    req to request

--- routes/users.route.js
+++ routes/users.route.js
@@ -165,7 +165,7 @@ router.post('/login', loginLimiter, doubleCsrfProtection, async (request, respon
             const redirect = request.body.redirect;
             const redirectIfUrl = '/cart';
             const redirectElseUrl = '/online-orders';
-            const rightUrl = (req.session.cart && req.session.cart.length > 0) ? redirectIfUrl : redirectElseUrl;
+            const rightUrl = (request.session.cart && request.session.cart.length > 0) ? redirectIfUrl : redirectElseUrl;
 
             response.send({
                 redirectURL : redirect ? `/${redirect}` : rightUrl, 
